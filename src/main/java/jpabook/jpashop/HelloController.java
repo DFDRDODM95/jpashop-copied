fa18da0f0c0f27f95274c9dfa52d628a3939d4e1
dfdrdodm95 <dfdrdodm95@gmail.com>
2024-01-28 11:51:02 +0900

    add MemberRepositoryTest

--- src/main/java/jpabook/jpashop/HelloController.java
+++ src/main/java/jpabook/jpashop/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
-    @GetMapping("Hello")
+    @GetMapping("hello")
     public String Hello(Model model) {
         model.addAttribute("data", "hello!!!");
         return "hello";
