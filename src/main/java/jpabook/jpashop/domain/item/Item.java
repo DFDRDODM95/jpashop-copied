dd5529bbe423b969909bf34e98afd1b5df24f07f
dfdrdodm95 <dfdrdodm95@gmail.com>
2024-02-06 00:49:04 +0900

    test:: order test

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -4,13 +4,14 @@ import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
 import jpabook.jpashop.exception.NotEnougthStockException;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@Getter
+@Getter @Setter
 public abstract class Item {
     @Id
     @GeneratedValue
